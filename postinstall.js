d8b9da181fe3758a2f03e903b4b54b428e044d02
Bryan Phelps <bryphe@outlook.com>
2018-06-22 16:46:24 -0700

    Fix typo in postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,7 @@
 const os = require("os")
 
 if (os.platform() === "win32") {
-    console.log("TODO: Install Cygwin)
+    console.log("TODO: Install Cygwin")
 } else {
     console.log("Not Windows; Cygwin is not required.")
 }
