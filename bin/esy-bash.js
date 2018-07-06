4d650eb28b6cfcd6112ece3017617f294f72b260
Bryan Phelps <bryphe@outlook.com>
2018-07-06 11:04:20 -0700

    Add arg sanitization to esy bash

--- bin/esy-bash.js
+++ bin/esy-bash.js
@@ -12,8 +12,11 @@ if (process.argv.length >= 3 && process.argv[2] === "--env") {
     }
 }
 
-const args = opts ? process.argv.slice(4).join(" ") : process.argv.slice(2).join(" ")
-console.log(args)
+
+const argsToSend = opts ? process.argv.slice(4) : process.argv.slice(2);
+const sanitizeArgs = (args) => args.map(a => "\"" + a + "\"").join(" ");
+
+const args = sanitizeArgs(argsToSend)
 
 bashExec(args, opts).then((code) => {
     process.exit(code)
