bbd6c16014eb90a5f28f86645c57a74259faf438
Bryan Phelps <bryphe@outlook.com>
2018-09-20 09:31:46 -0700

    Use new hash method

--- bash-exec.js
+++ bash-exec.js
@@ -32,8 +32,23 @@ const remapPathsInEnvironment = (env) => {
     return val
 }
 
+// From: https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+const getHashCode = (str) => {
+    var hash = 0, i, chr;
+  if (str.length === 0) return hash;
+  for (i = 0; i < str.length; i++) {
+          chr   = str.charCodeAt(i);
+          hash  = ((hash << 5) - hash) + chr;
+          hash |= 0; // Convert to 32bit integer
+    }
+  return hash;
+}
+
 const getUniqueId = (bashCommand) => {
-    return new Date().getTime();
+    const hash = getHashCode(bashCommand);
+    const time = new Date().getTime();
+
+    return `${hash.toString()}_${time.toString()}`
 }
 
 const bashExec = (bashCommand, options) => {
