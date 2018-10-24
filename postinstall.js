eec3033926f967527436e5454bf0cec23850fe5f
Bryan Phelps <bryphe@outlook.com>
2018-10-24 13:11:02 -0700

    Fix postinstall script

--- postinstall.js
+++ postinstall.js
@@ -4,7 +4,14 @@ if (process.platform !== "win32") {
     return 0;
 }
 
+const fs = require("fs");
+const path = require("path");
 const { restoreLinks } = require("./scripts/consolidate-links");
+
+if (!fs.existsSync(path.join(__dirname, ".cygwin"))) {
+    console.warn("No cygwin folder found, not restoring links");
+    return 0;
+}
 
 console.log("Restoring hardlinks...");
 restoreLinks();
