669d891e6dacd60c1f44b52fed9e9f4fbd9267d4
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2021-12-14 18:59:07 +0530

    Integrating work from previous contributors
    Co-authored-by: Bryan Phelps <bryphe@outlook.com>

--- postinstall.js
+++ postinstall.js
@@ -1,99 +1,139 @@
-const os = require("os")
-const path = require("path")
-const mkdirp = require("mkdirp")
-const download = require("download")
-const cp = require("child_process")
+const os = require("os");
+const path = require("path");
+const mkdirp = require("mkdirp");
+const download = require("download");
+const cp = require("child_process");
+const rimraf = require("rimraf");
+const fs = require("fs-extra");
 
-const log = (msg) => console.log(msg)
+const log = (msg) => console.log(msg);
 
 const install = async () => {
-    // Several of these constants are borrowed from:
-    // https://github.com/ocaml/ocaml-ci-scripts/blob/master/appveyor-install.ps1
+  // Several of these constants are borrowed from:
+  // https://github.com/ocaml/ocaml-ci-scripts/blob/master/appveyor-install.ps1
 
-    const cygSetup = "setup-x86_64.exe"
-    const cygDownloadPath = `https://cygwin.com/${cygSetup}`
-    const cygMirror = "http://cygwin.mirror.constant.com"
+  const cygSetup = "setup-x86_64.exe";
+  const cygDownloadPath = `https://cygwin.com/${cygSetup}`;
+  const cygMirror = "http://cygwin.mirror.constant.com";
 
-    const packagesToInstall = [
-        // Needed for cross-compilation to Windows native executables
-        "mingw64-x86_64-gcc-core",
-        "mingw64-x86_64-gcc-g++",
-        "mingw64-x86_64-headers",
-        "mingw64-x86_64-runtime",
-        "mingw64-x86_64-winpthreads",
+  const packagesToInstall = [
+    // Needed for cross-compilation to Windows native executables
+    "mingw64-x86_64-gcc-core",
+    "mingw64-x86_64-gcc-g++",
+    "mingw64-x86_64-headers",
+    "mingw64-x86_64-runtime",
+    "mingw64-x86_64-winpthreads",
 
-        // Linux utilities - 'bashisms' to support development
-        "curl",
-        "diff",
-        "diffutils",
-        "git",
-        "m4",
-        "make",
-        "patch",
-        "perl",
-        "rsync",
-        "unzip",
+    // Linux utilities - 'bashisms' to support development
+    "curl",
+    "diff",
+    "diffutils",
+    "git",
+    "m4",
+    "make",
+    "patch",
+    "perl",
+    "rsync",
+    "unzip",
+    "python",
+    "bash",
 
-        // Needed for installing the cygwin-build of OCaml
-        // May not be needed
-        // "gcc-g++",
-        // "flexdll",
-    ]
+    // Needed for installing the cygwin-build of OCaml
+    // May not be needed
+    // "gcc-g++",
+    // "flexdll",
+  ];
 
-    const destinationFolder = path.join(__dirname, ".cygwin")
-    const localPackageDirectory = path.join(destinationFolder, "var", "cache", "setup")
-    const cygSetupPath = path.join(destinationFolder, cygSetup)
+  const destinationFolder = path.join(__dirname, ".cygwin");
+  const localPackageDirectory = path.join(
+    destinationFolder,
+    "var",
+    "cache",
+    "setup"
+  );
+  const cygSetupPath = path.join(destinationFolder, cygSetup);
 
-    log(`Creating folder: ${destinationFolder}`)
-    mkdirp.sync(destinationFolder)
+  log(`Creating folder: ${destinationFolder}`);
+  mkdirp.sync(destinationFolder);
 
-    log(`Downloading setup from: ${cygDownloadPath} to: ${destinationFolder}`)
-    await download(cygDownloadPath, destinationFolder)
-    log(`Download complete!`)
+  log(`Downloading setup from: ${cygDownloadPath} to: ${destinationFolder}`);
+  await download(cygDownloadPath, destinationFolder);
+  log(`Download complete!`);
 
-    log(`Installing packages...`)
-    cp.spawnSync(cygSetupPath, [
-        "-qWnNdO",
-        "-R",
-        destinationFolder,
-        "-s",
-        cygMirror,
-        "-l",
-        localPackageDirectory,
-        "-P",
-        packagesToInstall.join(",") 
-    ], {
-        stdio: [process.stdin, process.stdout, process.stderr],
-        encoding: "utf-8",
-    })
+  log(`Installing packages...`);
+  cp.spawnSync(
+    cygSetupPath,
+    [
+      "-qWnNdO",
+      "-R",
+      destinationFolder,
+      "-s",
+      cygMirror,
+      "-l",
+      localPackageDirectory,
+      "-P",
+      packagesToInstall.join(","),
+    ],
+    {
+      stdio: [process.stdin, process.stdout, process.stderr],
+      encoding: "utf-8",
+    }
+  );
 
-    log(`Installation complete!`)
+  log(`Installation complete!`);
 
-    // Temporarily remove OPAM setting - 
-    // this should only be including in the `esy` bootstrapping.
-    // Long-term, we shouldn't actually need this, since `esy` can handle the install
-    // and loading the dependencies for us.
+  // Temporarily remove OPAM setting -
+  // this should only be including in the `esy` bootstrapping.
+  // Long-term, we shouldn't actually need this, since `esy` can handle the install
+  // and loading the dependencies for us.
 
-    // log(`Setting up OPAM...`)
-    // const bashExecutablePath = path.join(destinationFolder, "bin", "bash.exe")
-    // const opamScriptPath = path.resolve(path.join(__dirname, "install-opam.sh"))
+  // log(`Setting up OPAM...`)
+  // const bashExecutablePath = path.join(destinationFolder, "bin", "bash.exe")
+  // const opamScriptPath = path.resolve(path.join(__dirname, "install-opam.sh"))
 
-    // cp.spawnSync(bashExecutablePath, [
-    //     "-l",
-    //     opamScriptPath,
-    // ], {
-    //     stdio: "inherit",
-    //     encoding: "utf-8",
-    //     env: {
-    //         ...process.env
-    //     }
-    // })
+  // cp.spawnSync(bashExecutablePath, [
+  //     "-l",
+  //     opamScriptPath,
+  // ], {
+  //     stdio: "inherit",
+  //     encoding: "utf-8",
+  //     env: {
+  //         ...process.env
+  //     }
+  // })
 
-    // log(`OPAM setup complete`)
-}
+  // log(`OPAM setup complete`)
+
+  // Delete the /var/cache folder, since it's large and we don't need the cache at this point
+  console.log("Deleting /var/cache...");
+  rimraf.sync(path.join(__dirname, ".cygwin", "var", "cache"));
+  console.log("Deletion successful!");
+
+  // Explicitly set home directory
+  try {
+    fs.appendFileSync(
+      path.join(__dirname, ".cygwin", "etc", "nsswitch.conf"),
+      "\ndb_home: /usr/esy\n"
+    );
+  } catch (e) {
+    console.error("Something went wrong while updating nsswitch.conf");
+  }
+
+  // Run a command to test it out & create initial script files
+  cp.spawnSync(
+    path.join(__dirname, "re", "_build", "default", "bin", "EsyBash.exe"),
+    ["bash", "-lc", "cd ~ && pwd"]
+  );
+
+  console.log("Verifying esy profile set up...");
+  const bashRcContents = fs
+    .readFileSync(path.join(__dirname, ".cygwin", "usr", "esy", ".bashrc"))
+    .toString("utf8");
+  console.log("Esy user profile setup!");
+};
 
 if (os.platform() === "win32") {
-    install()
+  install();
 } else {
-    console.log("Not Windows; Cygwin is not required.")
+  console.log("Not Windows; Cygwin is not required.");
 }
