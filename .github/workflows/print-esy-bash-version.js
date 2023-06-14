e366042a91dae2360099dc3588751f4ad41dc672
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-06-14 11:08:36 +0530

    CI: Use one action step to compute all job variables

--- .github/workflows/print-esy-bash-version.js
+++ .github/workflows/print-esy-bash-version.js
@@ -1,2 +1,21 @@
-module.exports = exports = (core, packageJsonPath) =>
-  core.setOutput("version", require(packageJsonPath).version);
+const childProcess = require("child_process");
+module.exports = exports = (core, packageJsonPath) => {
+  let version = require(packageJsonPath).version;
+  let name = require(packageJsonPath).version;
+  let npmCachePath;
+  try {
+    npmCachePath = childProcess.execSync("npm config get cache");
+  } catch (e) {
+    if (process.platform === "win32") {
+      npmCachePath = path.join(process.env.LocalAppData, "npm-cache");
+    } else {
+      npmCachePath = path.join(process.env.HOME, ".npm");
+    }
+  }
+  core.setOutput("version", version);
+  core.setOutput(
+    "tarball",
+    `${name.replace("@", "").replace("/", "-")}-${version}.tgz`
+  );
+  core.setOutput("npm-cache-path", npmCachePath);
+};
