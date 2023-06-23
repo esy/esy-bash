41158ec6feac912e963352eab69a0846031dacf7
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-06-23 19:55:15 +0530

    CI: Use ${{ runner.temp }}

--- package.ps1
+++ package.ps1
@@ -1,14 +1,24 @@
+param([String] $TempDir)
+
 $ErrorActionPreference = "Stop"
 
 filter timestamp {"$(Get-Date -Format o): $_"}
 
 function New-TemporaryDirectory {
-    $parent = [System.IO.Path]::GetTempPath()
+    param([String] $TempDir)
+    if (!$TempDir) {
+	$parent = [System.IO.Path]::GetTempPath()
+    } else {
+	$parent = $TempDir
+    }
     [string] $name = [System.Guid]::NewGuid()
     New-Item -ItemType Directory -Path (Join-Path $parent $name)
 }
 
 function Install-In-Test-Project {
+
+    param([String] $TempDir)
+
     $PackageJson = Get-Content ./package.json -Raw | ConvertFrom-Json
     $PackageNamespace, $PackageName = $PackageJson.name.split("/")
 
@@ -25,10 +35,10 @@ function Install-In-Test-Project {
 
     $FullTarballPath = Resolve-Path($TarballName)
     echo "Tarball Path: $FullTarballPath"
-    $TempDir = New-TemporaryDirectory
-    echo "Changing directory to ${TempDir}"
-    cd $TempDir
-    $PackageJsonPath = Join-Path $TempDir "package.json"
+    $TestProjectDir = New-TemporaryDirectory $TempDir
+    echo "Changing directory to ${TestProjectDir}"
+    cd $TestProjectDir
+    $PackageJsonPath = Join-Path $TestProjectDir "package.json"
     echo "{}" > $PackageJsonPath
     echo "Will run: npm i ${FullTarballPath}"
     npm i $FullTarballPath
@@ -36,7 +46,7 @@ function Install-In-Test-Project {
     if ($PackageNamespace) {
 	$NodemodulePath = Join-Path $PackageNamespace $PackageName
     }
-    return Join-Path (Join-Path $TempDir "node_modules") $NodemodulePath 
+    return Join-Path (Join-Path $TestProjectDir "node_modules") $NodemodulePath 
 }
 
 echo "Building EsyBash.exe"
@@ -46,7 +56,7 @@ npm run download-packages | timestamp
 # npm run test-exe # Skipped because inline tests dont work on Windows without sys/time.h
 echo "NPM packing"
 npm pack | timestamp
-$TestProjectEsyBashPath = Install-In-Test-Project
+$TestProjectEsyBashPath = Install-In-Test-Project -TempDir $TempDir
 cd $TestProjectEsyBashPath
 echo "npm run test"
 npm run test | timestamp
