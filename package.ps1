dcaf4abbb9eeefa738e1f39bf8eeac095170b75c
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-06-23 17:40:11 +0530

    Run tests on tarball installed on a test project
    It's easier if the test project is local to source tree when it comes
    to Windows CI. We otherwise see 'permissions denied' issues

--- package.ps1
+++ package.ps1
@@ -1,3 +1,7 @@
+param([String] $TempDir)
+
+$ErrorActionPreference = "Stop"
+
 filter timestamp {"$(Get-Date -Format o): $_"}
 
 function Run {
@@ -14,6 +18,35 @@ function Run {
     }
 }
 
+# We have to run this for every test run because
+# tarball name changes for every commit on the CI
+# (Because of the commit hash in the version)
+function Install-In-Test-Project {
+
+    param([String] $TempDir)
+
+    $PackageJson = Get-Content ./package.json -Raw | ConvertFrom-Json
+    $PackageJsonName = $PackageJson.name
+    $PackageNamespace, $PackageName = $PackageJsonName.split("/")
+
+    if (!$PackageName) {
+	$PackageName = $PackageNamespace
+	$PackageNamespace = ""
+    }
+    $PackageVersion = $PackageJson.version
+    $TarballName = "$PackageName-$PackageVersion.tgz"
+    if ($PackageNamespace) {
+	$PackageNamespaceWithoutAt = $PackageNamespace.replace('@', '')
+	$TarballName = "$PackageNamespaceWithoutAt-$TarballName"
+    }
+
+    $FullTarballPath = Resolve-Path($TarballName)
+    echo "Tarball Path: $FullTarballPath"
+    cd ./test-project
+    npm i $FullTarballPath
+    cd ..
+}
+
 echo "Building EsyBash.exe"
 Run npm run build-exe
 
@@ -24,9 +57,9 @@ Run npm run download-packages
 
 echo "NPM packing"
 Run npm pack
-
-echo "node postinstall.js"
-Run node postinstall.js
-
+Install-In-Test-Project -TempDir $TempDir
+cd "./test-project"
+echo "npm i"
+Run npm i
 echo "npm run test"
 Run npm run test
